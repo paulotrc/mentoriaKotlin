f2f337f45cc50b16445a6522db28c0c39a55919c
Paulo Carneiro <paulotrc@gmail.com>
2023-12-17 17:04:02 -0300

    [NEW] Primeiro Commit

---
+++ svcVendaConsultaProduto/src/main/kotlin/br/paulotrc/svcvendaconsultaproduto/SvcVendaConsultaProdutoApplication.kt
@@ -0,0 +1,11 @@
+package br.paulotrc.svcvendaconsultaproduto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SvcVendaConsultaProdutoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SvcVendaConsultaProdutoApplication>(*args)
+}
---
+++ svcVendaConsultaProduto/src/test/kotlin/br/paulotrc/svcvendaconsultaproduto/SvcVendaConsultaProdutoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.paulotrc.svcvendaconsultaproduto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SvcVendaConsultaProdutoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
